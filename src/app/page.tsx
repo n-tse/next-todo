d24b0c986429543fa533082940321ab9b3f39184
Nick Tse <nicholashtse@gmail.com>
2023-08-13 18:38:59 -0400

    add event handler to toggle todo checked and update prisma

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,11 @@ const getTodos = () => {
   return prisma.todo.findMany();
 }
 
+const toggleTodo = async (id: string, complete: boolean) => {
+  "use server"
+  await prisma.todo.update({ where: { id }, data: { complete }})
+}
+
 export default async function Home() {
   // const todos = [
   //   {id: 1, title: "todo1"},
@@ -25,14 +30,14 @@ export default async function Home() {
         <h1 className="text-2xl">To Do List</h1>
         <Link
           href="/new"
-          className="p-1 border border-white rounded-md bg-inherit focus-within:bg-slate-500 hover:bg-slate-500 outline-none"
+          className="py-1 px-3 border border-white rounded-md bg-inherit focus-within:bg-slate-500 hover:bg-slate-500 outline-none"
         >
           New
         </Link>
       </header>
       <ul>
         {todos.map(({ id, title, complete }) => {
-          return <ToDoItem id={id} title={title} complete={complete}/>;
+          return <ToDoItem key={id} id={id} title={title} complete={complete} toggleTodo={toggleTodo}/>;
           // return <li key={id}>{title}</li>;
         })}
       </ul>
--- src/components/ToDoItem.tsx
+++ src/components/ToDoItem.tsx
@@ -1,13 +1,16 @@
+"use client"
+
 type ToDoItemProps = {
   id: string
   title: string
-  complete: Boolean
+  complete: boolean
+  toggleTodo: (id: string, complete: boolean) => void
 }
 
-const ToDoItem = ({ id, title, complete }: ToDoItemProps) => {
+const ToDoItem = ({ id, title, complete, toggleTodo }: ToDoItemProps) => {
   return (
     <li className="flex gap-1 ml-4">
-      <input id={id} type="checkbox" className="cursor-pointer peer"/>
+      <input id={id} type="checkbox" defaultChecked={complete} className="cursor-pointer peer" onChange={e => toggleTodo(id, e.target.checked)}/>
       <label htmlFor={id} className="cursor-pointer peer-checked:line-through peer-checked:text-slate-300">{title}</label>
     </li>
   )
